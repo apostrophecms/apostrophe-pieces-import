e101d832e5be69fc0746091a6956f11726c562c6
Tom Boutell <tom@punkave.com>
2018-12-05 16:16:48 -0500

    unit test the results, and the update feature

--- test/test.js
+++ test/test.js
@@ -28,7 +28,9 @@ describe('apostrophe-pieces-import', function () {
         'products': {
           extend: 'apostrophe-pieces',
           name: 'product',
-          import: true
+          import: true,
+          alias: 'products',
+          sort: { title: 1 }
         },
 
         'test-as-admin': {
@@ -59,7 +61,6 @@ describe('apostrophe-pieces-import', function () {
   });
 
   it('generate test csv file of products', function () {
-    var total = 50;
     var products = _.map(_.range(0, 50), function(i) {
       return {
         title: 'Cheese #' + padInteger(i, 5),
@@ -110,12 +111,70 @@ describe('apostrophe-pieces-import', function () {
     }
   });
 
-  it('dump job data', function (done) {
-    apos.modules['apostrophe-jobs'].db.find({}).toArray(function(err, jobs) {
+  it('check results', function() {
+    return apos.products.find(apos.tasks.getReq()).toArray().then(function(pieces) {
+      assert(pieces.length === 50);
+      assert(pieces[0].title === 'Cheese #00000');
+    });
+  });
+
+  it('generate test csv file of updates', function () {
+    var products = _.map(_.range(0, 10), function(i) {
+      return {
+        title: 'Cheese Food #' + padInteger(i, 5),
+        'slug:key': 'cheese-' + padInteger(i, 5)
+      };
+    });
+    fs.writeFileSync(apos.rootDir + '/data/temp/test.csv', stringify(products, { header: true }));
+  });
+
+  it('start importing the updates', function (done) {
+    var req = apos.tasks.getReq();
+    request.post({
+      url: 'http://localhost:7780/modules/products/import',
+      formData: {
+        file: fs.createReadStream(apos.rootDir + '/data/temp/test.csv')
+      },
+      json: true
+    }, function(err, response, body) {
       assert(!err);
+      assert(response.statusCode === 200);
+      assert(body.status === 'ok');
+      jobId = body._id;
       done();
     });
   });
+
+  it('import of updates concludes within 4 seconds', function (done) {
+    setTimeout(check, 4000);
+    function check() {
+      request.post({
+        url: 'http://localhost:7780/modules/products/import-progress',
+        json: true,
+        body: {
+          _id: jobId
+        }
+      }, function(err, response, body) {
+        assert(!err);
+        assert(response.statusCode === 200);
+        assert(body.status === 'ok');
+        assert(body.job.finished);
+        assert(!body.job.errors);
+        assert(body.job.processed === 10);
+        jobId = body._id;
+        done();
+      });
+    }
+  });
+
+  it('check results', function() {
+    return apos.products.find(apos.tasks.getReq()).toArray().then(function(pieces) {
+      assert(pieces.length === 50);
+      assert(_.find(pieces, { title: 'Cheese Food #00000' }));
+      assert(!_.find(pieces, { title: 'Cheese #00000' }));
+    });
+  });
+
 });
 
 
